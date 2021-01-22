01b8d590c8b60b808c4935147138af9647f0f2ab
Oscar Luna <CaporalesLuna@gmail.com>
2021-01-21 19:06:09 -0800

    Create server, DB Connection, User model and routes

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const keyFiles = require("./config/keys.js");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Importing Routes
+const users=require('./routes/Users.js')
+
+//App Routes
+app.get("/",(req,res)=>{
+    res.send("Welcome to Chat Application")
+})
+app.use('/api/users',users)
+
+//DB Configuration
+const db = keyFiles.mongoURI;
+
+//Connecting To DB
+mongoose.connect(db,{useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("Connected to DB"))
+.catch((error) => console.log(error));
+
+// Port Configuration
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`Server running at ${port}`);
